753a8d5eaca063b0a41537bf685799298690547f
Afzal Chishti <afzal.chishti@ibm.com>
2021-01-05 12:46:08 -0500

    Fixing accessing of env variables

--- back-end/index.js
+++ back-end/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// REACT_APP_HOST=capstone.ct6ijyqh0qoc.us-east-1.rds.amazonaws.com
+// REACT_APP_PORT=3306
+// REACT_APP_USER=admin
+// REACT_APP_PASSWORD=hinatashoyou
+// REACT_APP_DATABASE=problems
+
 const db = mysql.createPool({
   host: process.env.REACT_APP_HOST,
   port: process.env.REACT_APP_PORT,
   user: process.env.REACT_APP_USER,
   password: process.env.REACT_APP_PASSWORD,
   database: process.env.REACT_APP_DATABASE
-})
+});
 
-app.get('/', function (req, res) {
-  const sqlSearch = "SELECT * FROM questions"
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const sqlSearch = "SELECT * FROM questions;";
   db.query(sqlSearch, (err, result)=> {
-    res.send(result)
+    res.send(result);
   });
 });
 
