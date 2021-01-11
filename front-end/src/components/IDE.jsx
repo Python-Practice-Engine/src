379052377c5958edf955035dc6c3a55604a4c5d8
Afzal Chishti <afzal.chishti@ibm.com>
2021-01-11 15:30:51 -0500

    Hooking up db and frontend

--- front-end/src/components/IDE.jsx
+++ front-end/src/components/IDE.jsx
@@ -26,110 +26,60 @@ import '../style/style.css';
     },
   ];
   
-  const contentList = {
-    question: <QuestionContent 
-    title="Addition Calculator" 
-    tags="Functions"
-    description="
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
-    est laborum." />,
-    tutorial: <TutorialContent 
-    title="Functions" 
-    tags="Functions"
-    description={
-        [
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
-            est laborum.`, 
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
-            est laborum.`, 
-        ]
-    }
-    />,
-  };
-
-// const IDE = () => {
-//     const [question, setQuestion] = useState({});
-//     const [id, setID] = useState(0);
-
-//     useEffect(() => {
-//         // const { id } = this.props.match.params
-//         // Axios.get(`http://localhost:3001/questions/${id}`).then((response)=> {
-//         Axios.get(`http://localhost:3001/questions/1`).then((response)=> {
-//             console.log(response.data[0]);
-//             // setPosts
-//             // props.title=response.data[0].name;
-//             // props.tags=response.data[0].tags;
-//             // props.difficulty=response.data[0].difficulty;
-//             // props.description=response.data[0].description;
-//         });
-//     }, []);
-
-
-//     return (
-//         <div>
-//             {/* <Row gutter={32} style={{margin: "1% 5% 0 5%" }}>
-//                 <Col span={6} className="gutter-row">
-//                     <div className="question-col"> */}
-//                         {/* {questions.map(question => (
-//                             <Question 
-//                                 title={question.name}
-//                                 tags={question.tags}
-//                                 difficulty={question.difficulty}
-//                                 description={question.description}
-//                             />
-//                         ))} */}
-//                         <div>{question.name}</div>
-//                         {/* <Question 
-//                             title="Addition Calculator" 
-//                             tags="Functions" difficulty="Easy" 
-//                             description="Write a function which takes two parameters, adds them together, and returns the result"
-//                         /> */}
-//                     {/* </div>
-//                 </Col>
-//                 <Col span={18} className="gutter-row">
-//                         <Row gutter={16} >
-//                             <Skulpt ></Skulpt>    
-//                         </Row>
-//                         <Row gutter={16}>
-//                             <List
-//                                 style={{width:'100%', "margin-top": "1%"}}
-//                                 header={<h4>Test Cases</h4>}
-//                                 size="large"
-//                                 bordered
-//                                 dataSource={data}
-//                                 renderItem={item => <List.Item>
-//                                                         {item}
-//                                                         <span className="float-right"><Badge status="success" text="Passed" /></span>
-//                                                     </List.Item>}
-//                             />
-//                         </Row>         
-//                 </Col>
-//             </Row> */}
-//         </div>
-//     );
+//   const contentList = {
+//     question: <QuestionContent 
+//     title="Addition Calculator" 
+//     tags="Functions"
+//     description="
+//     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+//     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+//     veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+//     commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+//     velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+//     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+//     est laborum." />,
+//     tutorial: <TutorialContent 
+//     title="Functions" 
+//     tags="Functions"
+//     description={
+//         [
+//             `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+//             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+//             veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+//             commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+//             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+//             cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+//             est laborum.`, 
+//             `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+//             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+//             veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+//             commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+//             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+//             cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+//             est laborum.`, 
+//         ]
+//     }
+//     />,
+//   };
   class IDE extends React.Component  {
     state = {
         key: 'question',
+        question: {},
       };
     
       onTabChange = (key, type) => {
         this.setState({ [type]: key });
       };
+
+    componentDidMount(){
+        Axios.get(`http://localhost:3001/questions/1`).then((response)=> {
+            // console.log(response.data[0]);
+            this.setState({question: response.data[0]});
+            // console.log(this.state.question);
+            // this.state.question = response.data[0];
+            console.log(this.state.question)
+        });
+    } 
     
     render() {
         return (
@@ -145,7 +95,20 @@ import '../style/style.css';
                             this.onTabChange(key, 'key');
                             }}
                             > 
-                                {contentList[this.state.key]}
+                                {/* {contentList[this.state.key]} */}
+                                <QuestionContent 
+                                question={this.state.question} />
+                                {/* <QuestionContent 
+                                title={"Addition Calculator"} 
+                                tags="Functions"
+                                description="
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+                                est laborum." /> */}
                             </Card>
                             <TestCases></TestCases>
                         </Col>
--- front-end/src/components/QuestionContent.jsx
+++ front-end/src/components/QuestionContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
     Typography,
     Tag,
@@ -9,18 +9,32 @@ import '../style/style.css';
 const { Title } = Typography;
 
 function QuestionContent(props) {
+    
+
+    useEffect(() => {
+        // const { id } = this.props.match.params
+        // Axios.get(`http://localhost:3001/questions/${id}`).then((response)=> {
+        // Axios.get(`http://localhost:3001/questions/1`).then((response)=> {
+        //     console.log(response.data[0]);
+        //     // setPosts
+        // });
+        console.log("Props have changed");
+        
+    }, []);
+
+    console.log(props.question);
     return (
         <div>
             <Title 
                 level={3} 
             >
-                {props.title}
+                {props.question.name}
             </Title>
             <Space>
-                <Tag color="warning">{props.tags}</Tag>
+                <Tag color="warning">{props.question.tags}</Tag>
             </Space>
             <p className="questionDescription">
-                {props.description}
+                {props.question.description}
             </p> 
         </div>
     );
