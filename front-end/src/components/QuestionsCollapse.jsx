b7bc36d469506458986719f40774d2da9fd7cb19
Razi <syedru@mcmaster.ca>
2021-01-11 08:00:27 -0500

    flushed out questions page

--- front-end/src/components/QuestionsCollapse.jsx
+++ front-end/src/components/QuestionsCollapse.jsx
@@ -1,36 +1,118 @@
 import React from 'react';
 import {
-    Card,
-    Badge,
-    Space,
-    Tag,
-    Table,
-    Collapse
+    Collapse,
+    List,
+    Avatar,
+    Typography,
 } from 'antd';
 import '../style/style.css';
 import 'antd/dist/antd.css';
+import { CodeTwoTone } from '@ant-design/icons';
 
 const { Panel } = Collapse;
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
+const { Title } = Typography;
+
+const data = [
+    {
+        title: 'Question 1',
+    },
+    {
+        title: 'Question 2',
+    },
+    {
+        title: 'Question 3',
+    },
+    {
+        title: 'Question 4',
+    },
+];
 
 function QuestionsCollapse() {
     return (
-        <Collapse className="collapse" defaultActiveKey={['1']} ghost>
-            <Panel className="panel" header="This is panel header 1" key="1">
-            <p>{text}</p>
-            </Panel>
-            <Panel className="panel" header="This is panel header 2" key="2">
-            <p>{text}</p>
-            </Panel>
-            <Panel className="panel" header="This is panel header 3" key="3">
-            <p>{text}</p>
-            </Panel>
-        </Collapse>
+        <div className="collapse-topics">
+            <Collapse
+                ghost
+                expandIconPosition='right'
+            >
+                <Panel className="panel-topic" header={<Title level={4}>The Basics</Title>} key="1" >
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar icon={<CodeTwoTone twoToneColor="blue" />} style={{ backgroundColor: 'white' }} />}
+                                    title={<a href="https://ant.design">{item.title}</a>}
+                                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet congue lacus vitae vestibulum. Ut quis diam in nisl venenatis."
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Panel>
+                <Panel className="panel-topic" header={<Title level={4}>Data Types</Title>} key="2">
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar icon={<CodeTwoTone twoToneColor="blue" />} style={{ backgroundColor: 'white' }} />}
+                                    title={<a href="https://ant.design">{item.title}</a>}
+                                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet congue lacus vitae vestibulum. Ut quis diam in nisl venenatis."
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Panel>
+                <Panel className="panel-topic" header={<Title level={4}>Operators</Title>} key="3">
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar icon={<CodeTwoTone twoToneColor="blue" />} style={{ backgroundColor: 'white' }} />}
+                                    title={<a href="https://ant.design">{item.title}</a>}
+                                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet congue lacus vitae vestibulum. Ut quis diam in nisl venenatis."
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Panel>
+                <Panel className="panel-topic" header={<Title level={4}>Control Structures</Title>} key="4">
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar icon={<CodeTwoTone twoToneColor="blue" />} style={{ backgroundColor: 'white' }} />}
+                                    title={<a href="https://ant.design">{item.title}</a>}
+                                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet congue lacus vitae vestibulum. Ut quis diam in nisl venenatis."
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Panel>
+                <Panel className="panel-topic" header={<Title level={4}>Functions</Title>} key="5">
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar icon={<CodeTwoTone twoToneColor="blue" />} style={{ backgroundColor: 'white' }} />}
+                                    title={<a href="https://ant.design">{item.title}</a>}
+                                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean laoreet congue lacus vitae vestibulum. Ut quis diam in nisl venenatis."
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Panel>
+            </Collapse>
+        </div>
+
     );
 }
 
