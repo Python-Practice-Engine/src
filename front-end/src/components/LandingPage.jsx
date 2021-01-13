276b9c6794ace4a2570834d167b2d6bf19b880ae
noa-barsky <barskyn@mcmaster.ca>
2021-01-13 17:41:52 -0500

    removed lorem ipsum and removed two tabs

--- front-end/src/components/LandingPage.jsx
+++ front-end/src/components/LandingPage.jsx
@@ -15,10 +15,10 @@ const LandingPage = () => (
   <div>
     <Container>
       <Row>
-        <Col md={7}>
+        <Col md={6}>
           <Image src={landingpageImage} className="landingPageImg" />
         </Col>
-        <Col md={5}>
+        <Col md={6}>
           <Title
             level={2}
             className="landingPageTitle"
@@ -27,13 +27,30 @@ const LandingPage = () => (
             Welcome to Python Practice Engine!
           </Title>
           <Container>
-            <p className="landingPageDescription">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Purus gravida quis blandit turpis cursus in hac habitasse platea.
-              Sit amet dictum sit amet justo donec. Molestie a iaculis at erat
-              pellentesque adipiscing. Facilisis mauris sit amet massa vitae
-              tortor condimentum lacinia quis.
+            <p>
+              &nbsp;&nbsp;&nbsp;&nbsp;Python Practice Engine is an interactive
+              tool to be used by McMaster students learning how to code in
+              Python. How it works is: if you go click on the
+              &quot;Practice Engine&quot; tab you
+              will be recommended questions that start with more basic topics
+              and end with advanced programming topics.
+            </p>
+            <p>
+              &nbsp;&nbsp;&nbsp;&nbsp;The way questions are recommended is
+              organized by a concept
+              graph. The concept graph allows for a user to learn and
+              understand the fundamental topics before jumping to more advanced
+              ones.Python
+              Practice Engine allows you to train your programming muscle
+              through programming questions while also providing in-depth
+              tutorials on the concepts the question covers. If you do not want
+              to be recommended questions you can go straight to the
+              &quot;Questions&quot; tab and select specfic questions to do.
+            </p>
+            <p>
+              &nbsp;&nbsp;&nbsp;&nbsp;Python Practice Engine was created as a
+              capstone project
+              under the supervision of Dr. Brown and we hope you enjoy it!
             </p>
           </Container>
         </Col>
