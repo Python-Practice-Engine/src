1842f4faf32c0a659b017459302ee31f9ea28a9c
connorhewick <connor@hewick.ca>
2021-01-13 17:30:49 -0500

    ready to add question data to db

--- back-end/createTables.sql
+++ back-end/createTables.sql
@@ -2,7 +2,7 @@ use pythonlearning;
 
 CREATE TABLE Tutorials (
 	Tid INT NOT NULL AUTO_INCREMENT,
-	description VARCHAR(200) NOT NULL,
+	description VARCHAR(1000) NOT NULL,
 	code VARCHAR(100),
 	PRIMARY KEY ( Tid )
 );
@@ -11,7 +11,7 @@ CREATE TABLE Questions (
 	Qid INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(40) NOT NULL,
 	tags VARCHAR(50) NOT NULL,
-	description VARCHAR(200) NOT NULL,
+	description VARCHAR(1000) NOT NULL,
 	difficulty ENUM('easy','medium','hard'),
 	Tid INT,
 	PRIMARY KEY ( Qid ),
@@ -21,7 +21,8 @@ CREATE TABLE Questions (
 CREATE TABLE Testcases (
 	TCid INT NOT NULL AUTO_INCREMENT,
 	code VARCHAR(100),
-	Qid INT,
+	Qid INT NOT NULL,
 	PRIMARY KEY ( TCid ),
 	FOREIGN KEY (Qid) REFERENCES Questions(Qid)
 );
+
--- back-end/insertQuestions.sql
+++
@@ -1,3 +0,0 @@
-use pythonlearning;
-
-INSERT INTO Questions (name, description, tags, difficulty) values ( 'Addition Calculator', 'Write a function which takes two parameters, adds them together, and returns the result', 'functions', 'easy');
