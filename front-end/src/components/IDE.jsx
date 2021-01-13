6083e2bca8644ba00caac204768000d14ce08d5b
Razi Syed <syedru@mcmaster.ca>
2021-01-13 15:41:35 -0500

    Razi questions page (#21)
    * Cleaning up hardcoded values
    
    * Testing hookup of backend & frontend
    
    * Cleaning up redundant code
    
    * Move hooks to IDE component
    
    * Cleaning up
    
    * Adding state
    
    * Adding state to components
    
    * Hooking up db and frontend
    
    * Removing redundant useEffect
    
    * Dynamic question selection
    
    * Cleaning up files
    
    * added left button
    
    * Fixing linting issues
    
    * button editing
    
    * fix eslint errors
    
    * added right left button
    
    * Linking from questions page to IDE
    
    * Testing buttons on IDE page
    
    * uncommented onClick so app doesn't crash
    
    * fixed refresh on button
    
    Co-authored-by: Afzal Chishti <afzal.chishti@ibm.com>

--- front-end/src/components/IDE.jsx
+++ front-end/src/components/IDE.jsx
@@ -60,15 +60,28 @@ class IDE extends React.Component {
     this.state = {
       key: 'question',
       question: {},
+      id: this.props.match.params.id,
     };
+
+    this.handleHardClick = this.handleHardClick.bind(this);
+    this.handleEasyClick = this.handleEasyClick.bind(this);
   }
 
   componentDidMount() {
-    const { id } = this.props.match.params;
-    Axios.get(`http://localhost:3001/questions/${id}`).then((response) => {
+    Axios.get(`http://localhost:3001/questions/${this.state.id}`).then((response) => {
       this.setState({ question: response.data[0] });
       console.log(response.data);
     });
+  }
+
+  handleHardClick() {
+    this.setState({ id: this.props.match.params.id });
+    setTimeout(() => window.location.reload(), 300);
+  }
+
+  handleEasyClick() {
+    this.setState({ id: this.props.match.params.id });
+    setTimeout(() => window.location.reload(), 300);
   }
 
   onTabChange = (key, type) => {
@@ -98,7 +111,11 @@ class IDE extends React.Component {
               <TestCases />
             </Col>
             <Col md="6">
-              <Skulpt id={this.state.question.id} />
+              <Skulpt
+                id={this.state.question.id}
+                handleHardClick={this.handleHardClick}
+                handleEasyClick={this.handleEasyClick}
+              />
             </Col>
           </Row>
 
