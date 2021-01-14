4cda854fd560393c68324ef93773c55908ace847
elwazana <elwazana@mcmaster.ca>
2021-01-13 19:35:49 -0500

    Finished submit button functionality and added more code documentation to Skulpt and TestCases

--- front-end/src/components/TestCases.jsx
+++ front-end/src/components/TestCases.jsx
@@ -7,34 +7,48 @@ import {
 
 import '../style/style.css';
 
+// Variable declarations
 const { Title } = Typography;
-
-const data = [
-  'Test Case #1',
-  'Test Case #2',
-];
+let i;
+const j = 'Test Case #';
+const data = [];
 
 // Used to render the test-cases table in the IDE page
-const TestCases = () => (
-  <div className="testCases">
-    <List
-      style={{ width: '100%' }}
-      header={<Title level={4}>Test Cases</Title>}
-      size="large"
-      bordered
-      dataSource={data}
-      renderItem={
-        (item) => (
-          <List.Item>
-            {item}
-            <span className="float-right">
-              <Tag style={{ display: 'none' }} color="success">Passed</Tag>
-            </span>
-          </List.Item>
-        )
-      }
-    />
-  </div>
-);
+class TestCases extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Populate variable with retrieved test-cases
+    const tests = this.props.testCases;
+    for (i = 0; i < tests.length; i += 1) {
+      data.push(j + tests[i].TCid);
+    }
+  }
+
+  // Display the test-cases table on IDE page
+  render() {
+    return (
+      <div className="testCases">
+        <List
+          style={{ width: '100%' }}
+          header={<Title level={4}>Test Cases</Title>}
+          size="large"
+          bordered
+          dataSource={data}
+          renderItem={
+            (item) => (
+              <List.Item>
+                {item}
+                <span className="float-right">
+                  <Tag id={item} style={{ display: 'none' }} color="success">Passed</Tag>
+                </span>
+              </List.Item>
+            )
+          }
+        />
+      </div>
+    );
+  }
+}
 
 export default TestCases;
