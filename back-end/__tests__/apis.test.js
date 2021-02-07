6070509985e0fa726ac6cc7482f6f4a30691842c
connorhewick <connor@hewick.ca>
2021-02-06 22:51:53 -0500

    more tests added

--- back-end/__tests__/apis.test.js
+++ back-end/__tests__/apis.test.js
@@ -1,5 +1,5 @@
 describe("Tests on the Questions Table", () => {
-    test("print statements is easy", done => {
+    test("print statements is easy difficulty", done => {
       const users = global.db.query(
         "SELECT * FROM Questions WHERE tags='PrintStatement'",
         (error, results, fields) => {
@@ -14,7 +14,7 @@ describe("Tests on the Questions Table", () => {
       );
     });
 
-    test("the second question is variables", done => {
+    test("the second question is about variables", done => {
       const users = global.db.query(
         "SELECT * FROM Questions WHERE Qid=2",
         (error, results, fields) => {
@@ -27,5 +27,76 @@ describe("Tests on the Questions Table", () => {
         }
       );
     }); 
+
+    test("the third question is about numbers", done => {
+      const users = global.db.query(
+        "SELECT * FROM Questions WHERE Qid=3",
+        (error, results, fields) => {
+          if (error) {
+            throw error;
+          }
+          expect(results).toHaveLength(1);
+          expect(results[0].tags).toEqual("Numbers");
+          expect(results[0].Qid).toEqual(3);
+          done();
+        }
+      );
+    });
+
+    test("each question has a tutorial", done => {
+      const users = global.db.query(
+        "SELECT * FROM Questions",
+        (error, results, fields) => {
+          if (error) {
+            throw error;
+          }
+          for (const i in results) {
+            expect(results[i].Tid).toEqual(expect.any(Number));
+          }
+          done();
+        }
+      );
+    });
 });
 
+describe("Tests on the Tutorials Table", () => {
+    test("all tutorials have a Tid, name, and description", done => {
+      const users = global.db.query(
+        "SELECT * FROM Tutorials",
+        (error, results, fields) => {
+          if (error) {
+            throw error;
+          }
+          for (const i in results) {
+            expect(results[i].Tid).toBeGreaterThan(0);
+            expect(results[i].name).toEqual(expect.any(String));
+            expect(results[i].description).toEqual(expect.any(String));
+          }
+          done();
+        }
+      );
+    });
+
+    
+});
+
+describe("Tests on the Testcases Table", () => {
+    test("", done => {
+      const users = global.db.query(
+        "SELECT * FROM Tutorials",
+        (error, results, fields) => {
+          if (error) {
+            throw error;
+          }
+          for (const i in results) {
+            expect(results[i].Tid).toBeGreaterThan(0);
+            expect(results[i].name).toEqual(expect.any(String));
+            expect(results[i].description).toEqual(expect.any(String));
+          }
+          done();
+        }
+      );
+    });
+
+    
+});
