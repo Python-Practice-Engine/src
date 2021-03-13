3c3be1a00d9e8c31bd02c2dcc6bba63432253f97
Afzal Chishti <afzalmchishti@gmail.com>
2021-03-13 14:02:28 -0500

    Basic setup of concept map

---
+++ back-end/Concept-Map/Graph.js
@@ -0,0 +1,89 @@
+// need to create a dependency graph
+
+// create a graph class 
+// using adjacency list which requires less space, faster 
+class Graph { 
+    // defining vertex array and 
+    // adjacent list 
+    constructor(noOfVertices) { 
+        this.noOfVertices = noOfVertices; 
+        this.AdjList = new Map(); 
+    } 
+  
+    // functions to be implemented 
+  
+    // add vertex to the graph 
+    addVertex(v) { 
+        // initialize the adjacent list with a 
+        // null array 
+        this.AdjList.set(v, []); 
+    } 
+   
+    // add edge to the graph 
+    addEdge(v, w) { 
+        // get the list for vertex v and put the 
+        // vertex w denoting edge between v and w 
+        this.AdjList.get(v).push(w); 
+    } 
+    
+    // Prints the vertex and adjacency list 
+    printGraph() { 
+        // get all the vertices 
+        var get_keys = this.AdjList.keys(); 
+    
+        // iterate over the vertices 
+        for (var i of get_keys)  { 
+            // great the corresponding adjacency list 
+            // for the vertex 
+            var get_values = this.AdjList.get(i); 
+            var conc = ""; 
+    
+            // iterate over the adjacency list 
+            // concatenate the values into a string 
+            for (var j of get_values) 
+                conc += j + " "; 
+    
+            // print the vertex and its adjacency list 
+            console.log(i + " -> " + conc); 
+        } 
+    } 
+  
+    // function to performs BFS, key for the recommendation algorithm 
+    bfs(startingNode) { 
+
+        // create a visited object 
+        var visited = {}; 
+
+        // Create an object for queue 
+        var q = new Queue(); 
+
+        // add the starting node to the queue 
+        visited[startingNode] = true; 
+        q.enqueue(startingNode); 
+
+        // loop until queue is element 
+        while (!q.isEmpty()) { 
+            // get the element from the queue 
+            var getQueueElement = q.dequeue(); 
+
+            // passing the current vertex to callback funtion 
+            console.log(getQueueElement); 
+
+            // get the adjacent list for current vertex 
+            var get_List = this.AdjList.get(getQueueElement); 
+
+            // loop through the list and add the element to the 
+            // queue if it is not processed yet 
+            for (var i in get_List) { 
+                var neigh = get_List[i]; 
+
+                if (!visited[neigh]) { 
+                    visited[neigh] = true; 
+                    q.enqueue(neigh); 
+                } 
+            } 
+        } 
+    } 
+
+    // dfs(v) 
+} 
---
+++ back-end/Concept-Map/conceptMap.js
@@ -0,0 +1,27 @@
+const DirectedGraphMap = require('directed-graph-map');
+
+const dgm = new DirectedGraphMap([['Intro', 'Numbers', 'String', 'Boolean', 'Conditionals', 'If/else', 'For loops', 'While loops', 'Try and catch', 'List', 'Tuple', 'dict', 'Iterative Functions', 'Recursive Functions', 'Files Input and Output', 'Classes']]);
+
+dgm.hasEdge('Intro', 'Numbers');
+dgm.hasEdge('Intro', 'String');
+dgm.hasEdge('Intro', 'Boolean');
+
+dgm.hasEdge('Numbers', 'String');
+dgm.hasEdge('String', 'Boolean');
+
+// edges to list and dict once it has completed all 3 variables
+dgm.hasEdge('Numbers', 'List');
+dgm.hasEdge('String', 'List');
+dgm.hasEdge('Boolean', 'List');
+
+dgm.hasEdge('Numbers', 'dict');
+dgm.hasEdge('String', 'dict');
+dgm.hasEdge('Boolean', 'dict');
+
+dgm.hasEdge('Boolean', 'If/else');
+
+dgm.hasEdge('String', 'For loops');
+dgm.hasEdge('String', 'While loops');
+
+dgm.hasEdge('For loops', 'While loops');
+// dgm.hasEdge('While loops', 'For loops');
