b8c04ae9f1b955c9be66c8fb03b90757b4535132
Afzal Chishti <afzalmchishti@gmail.com>
2021-03-25 11:23:57 -0400

    firebase

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,13 +7,40 @@ import 'codemirror/theme/icecoder.css';
 import './style/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 // Personal component import
 import Outline from './components/Outline';
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyA9eJDXGjWHXXBboWbflw_HNHocR_4sIIU',
+  authDomain: 'python-learning-87aa4.firebaseapp.com',
+  projectId: 'python-learning-87aa4',
+  storageBucket: 'python-learning-87aa4.appspot.com',
+  messagingSenderId: '385323662435',
+  appId: '1:385323662435:web:1b99a3b85221a158850c56',
+  measurementId: 'G-WK5GEQ4YW2',
+});
+
+const [user] = useAuthState(firebase.auth());
+
 ReactDOM.render(
   <React.StrictMode>
-    <Outline />
+    {user ? <Outline /> : <SignIn /> }
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
+
+  return (
+    <button onClick={signInWithGoogle} type="button"> Sign in with Google</button>
+  );
+}
