70fac60c3a0a1fef4d53ac111e59cfcc09668f38
connorhewick <connor@hewick.ca>
2021-03-27 21:02:53 -0400

    sql injection fix by using prepared queries

--- back-end/app.js
+++ back-end/app.js
@@ -22,8 +22,8 @@ app.use(express.json());
 // Route for retrieving questions based on concepts
 app.get('/questionSet/:tags', (req, res) => {
   var tags = req.params.tags;
-  const sqlSearch = `SELECT * FROM Questions WHERE tags = '${tags}';`;
-  db.query(sqlSearch, (err, result)=> {
+  const sqlSearch = `SELECT * FROM Questions WHERE tags = ?;`;
+  db.query(sqlSearch, [tags], (err, result)=> {
     res.send(result);
   });
 });
@@ -33,8 +33,8 @@ app.get('/questionSet/:tags', (req, res) => {
 app.get('/questions/:Qid', (req, res) => {
   // Retrieve the tag from our URL path
   var Qid = req.params.Qid;
-  const sqlRetrieve = `SELECT * FROM Questions WHERE Qid = ${Qid}`;
-  db.query(sqlRetrieve, (err, result)=> {
+  const sqlRetrieve = `SELECT * FROM Questions WHERE Qid = ?;`;
+  db.query(sqlRetrieve, [Qid], (err, result)=> {
     res.send(result);
   });
 });
@@ -44,8 +44,8 @@ app.get('/questions/:Qid', (req, res) => {
 app.get('/testcases/:Qid', (req, res) => {
   // Retrieve the question id from our URL path
   var Qid = req.params.Qid;
-  const sqlRetrieve = `SELECT * FROM Testcases WHERE Qid = ${Qid};`;
-  db.query(sqlRetrieve, (err, result)=> {
+  const sqlRetrieve = `SELECT * FROM Testcases WHERE Qid = ?;`;
+  db.query(sqlRetrieve, [Qid], (err, result)=> {
     res.send(result);
   });
 });
@@ -54,8 +54,8 @@ app.get('/testcases/:Qid', (req, res) => {
 // Route for retrieving testcases related to a question
 app.get('/tutorial/:Tid', (req,res) => {
   var Tid = req.params.Tid;
-  const sqlRetrieve = `SELECT * FROM Tutorials WHERE Tid = ${Tid} OR Tid = -1 ORDER BY Tid DESC LIMIT 1;`;
-  db.query(sqlRetrieve, (err, result)=> {
+  const sqlRetrieve = `SELECT * FROM Tutorials WHERE Tid = ? OR Tid = -1 ORDER BY Tid DESC LIMIT 1;`;
+  db.query(sqlRetrieve, [Tid], (err, result)=> {
     res.send(result);
   });
 });
